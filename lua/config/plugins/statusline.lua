79e749cac79aa1766038acae8cd7d037e374f5f1
jiangfuxiong_i <jiangfuxiong_i@didiglobal.com>
2023-07-25 16:34:03 +0800

    feature:[stateline emoji]

--- lua/config/plugins/statusline.lua
+++ lua/config/plugins/statusline.lua
@@ -1,3 +1,23 @@
+-- Define a custom component that includes an emoji based on the current mode
+local function mode_emoji()
+  local mode = vim.fn.mode()
+  local emoji = ""
+  if mode == "n" then
+    emoji = "🤔"
+  elseif mode == "i" then
+    emoji = "🥰"
+  elseif mode == "v" then
+    emoji = "😈"
+  elseif mode == "V" then
+    emoji = "👿"
+  elseif mode == "c" then
+		emoji = "🤪"
+  elseif mode == "t" then
+    emoji = "😑"
+  end
+  return emoji
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	-- You can optionally lazy-load heirline on UiEnter
@@ -26,7 +46,7 @@ return {
 			sections = {
 				lualine_a = { 'filename' },
 				lualine_b = { 'branch', 'diff', 'diagnostics' },
-				lualine_c = {},
+				lualine_c = { mode_emoji },
 				lualine_x = {},
 				lualine_y = { 'filesize', 'fileformat', 'filetype' },
 				lualine_z = { 'location' }
