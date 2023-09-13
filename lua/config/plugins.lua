31d955bb1abc7665d115c4988e48bee713dc2d6c
jiangfuxiong_i <jiangfuxiong_i@didiglobal.com>
2023-09-13 13:05:30 +0800

    feature:[dropbar add]

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
 	require("config.plugins.tabline"),
 	require("config.plugins.wilder"),
 	require("config.plugins.joshuto"),
-	-- require("config.plugins.winbar"),
+	require("config.plugins.winbar"),
 	require("config.plugins.move-motion"),
 	require("config.plugins.surround"),
 	require("config.plugins.comment"),
